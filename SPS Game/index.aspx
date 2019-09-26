b461ca94b18e1c0a50e7e07780e9664661b72f28
Bryan <Bryan@DESKTOP-VT7NU1M>
2019-09-26 14:46:33 +0800

    Add project files.

---
+++ SPS Game/index.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="SPS_Game.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+
+
+        Scissors, Paper, Stone Game!<br />
+        <br />
+        Rules: You have to select either &#39;Scissors&#39;, &#39;Paper&#39; or &#39;Stone&#39;. Click on the respective buttons. Upon clicking, the computer will make it&#39;s choice and you will find out<br />
+        whether or not you have won! Once the game has ended, click on the &#39;Restart&#39; button to play again.<br />
+        <br />
+        Scissors &gt; Paper<br />
+        Paper &gt; Stone<br />
+        Stone &gt; Scissors<br />
+        <br />
+        <br />
+        Your Pick:<br />
+        <asp:Button ID="btn_Scs" runat="server" OnClick="btn_Scs_Click" Text="Scissors" />
+&nbsp;<asp:Button ID="btn_Paper" runat="server" OnClick="btn_Paper_Click" Text="Paper" />
+&nbsp;<asp:Button ID="btn_Stone" runat="server" OnClick="btn_Stone_Click" Text="Stone" />
+        <br />
+        <br />
+        <br />
+        Computer&#39;s Pick:<br />
+        <asp:Label ID="lbl_ComputerPick" runat="server"></asp:Label>
+        <br />
+        <br />
+        <br />
+        Result:
+        <asp:Label ID="lbl_Result" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:Button ID="btn_Restart" runat="server" OnClick="btn_Restart_Click" Text="Restart" />
+    
+
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ SPS Game/index.aspx.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SPS_Game
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            btn_Restart.Visible = false;
+        }
+
+        protected void btn_Scs_Click(object sender, EventArgs e)
+        {
+            randomizer();
+            determineResult("Scissors");
+            btn_Paper.Enabled = false;
+            btn_Scs.Enabled = false;
+            btn_Stone.Enabled = false;
+            btn_Restart.Visible = true;
+        }
+
+        protected void btn_Paper_Click(object sender, EventArgs e)
+        {
+            randomizer();
+            determineResult("Paper");
+            btn_Paper.Enabled = false;
+            btn_Scs.Enabled = false;
+            btn_Stone.Enabled = false;
+            btn_Restart.Visible = true;
+        }
+
+        protected void btn_Stone_Click(object sender, EventArgs e)
+        {
+            randomizer();
+            determineResult("Stone");
+            btn_Paper.Enabled = false;
+            btn_Scs.Enabled = false;
+            btn_Stone.Enabled = false;
+            btn_Restart.Visible = true;
+        }
+
+        private void randomizer()
+        {
+            Random random = new Random();
+            string[] list = new string[3] { "Scissors", "Paper", "Stone" };
+            int index = random.Next(0, list.Length);
+            lbl_ComputerPick.Text = list[index].ToString().Trim();
+        }
+
+        private void determineResult(string userChoice)
+        {
+            string compPick = lbl_ComputerPick.Text.Trim();
+
+            if (string.Equals(userChoice, "Scissors"))
+            {
+                if (string.Equals(compPick, "Scissors"))
+                {
+                    lbl_Result.Text = "Tie. User picked: " + userChoice + " and the Computer picked: " + compPick;
+                }
+                else if (string.Equals(compPick, "Paper"))
+                {
+                    lbl_Result.Text = "You won. User picked: " + userChoice + " and the Computer picked: " + compPick;
+                }
+                else if (string.Equals(compPick, "Stone"))
+                {
+                    lbl_Result.Text = "You lost. User picked: " + userChoice + " and the Computer picked: " + compPick;
+                }
+            }
+            else if (string.Equals(userChoice, "Paper"))
+            {
+                if (string.Equals(compPick, "Scissors"))
+                {
+                    lbl_Result.Text = "You lost. User picked: " + userChoice + " and the Computer picked: " + compPick;
+                }
+                else if (string.Equals(compPick, "Paper"))
+                {
+                    lbl_Result.Text = "Tie. User picked: " + userChoice + " and the Computer picked: " + compPick;
+                }
+                else if (string.Equals(compPick, "Stone"))
+                {
+                    lbl_Result.Text = "You won. User picked: " + userChoice + " and the Computer picked: " + compPick;
+                }
+            }
+
+            else if (string.Equals(userChoice, "Stone"))
+            {
+                if (string.Equals(compPick, "Scissors"))
+                {
+                    lbl_Result.Text = "You won. User picked: " + userChoice + " and the Computer picked: " + compPick;
+                }
+                else if (string.Equals(compPick, "Paper"))
+                {
+                    lbl_Result.Text = "You lost. User picked: " + userChoice + " and the Computer picked: " + compPick;
+                }
+                else if (string.Equals(compPick, "Stone"))
+                {
+                    lbl_Result.Text = "Tie. User picked: " + userChoice + " and the Computer picked: " + compPick;
+                }
+            }
+
+        }
+
+        protected void btn_Restart_Click(object sender, EventArgs e)
+        {
+            lbl_ComputerPick.Text = "";
+            lbl_Result.Text = "";
+            btn_Paper.Enabled = true;
+            btn_Scs.Enabled = true;
+            btn_Stone.Enabled = true;
+            btn_Restart.Visible = false;
+        }
+    }
+}
